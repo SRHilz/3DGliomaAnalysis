3dbc0289719758e298df966de6618dbc9b881fa0
srhilz <stephanie.hilz@gmail.com>
2021-09-28 22:48:38 -0700

    updated to use pyclone input

--- scripts/SID000014_mutational_categorization_clonal_subclonal_by_patient.R
+++ scripts/SID000014_mutational_categorization_clonal_subclonal_by_patient.R
@@ -85,27 +85,33 @@ source('/Users/shilz/Documents/Professional/Positions/UCSF_Costello/Publications
 # specify cutoffs - which patients to look at, how many samples will be used, and how pure each must be;
 #  if there are more samples than samplesPerPatientToUse, will average result from all possible combinations
 #  of this number.
-purityCutoff <- .3
+exploratoryPurityCutoff <- .3 # used at the very beginging just to get a sense of what is tumor-wide
 samplesPerPatientToUse <- 2 #can also be NA; in this case all samples fitting the criteria will be used
+pycloneCellPrevCutoff <- 0
 requireSM <- FALSE
 
 # read in sample data file
-merged <- read.table(sampleDataFile, sep='\t', header = T, stringsAsFactors = F)
+sampleData <- read.table(sampleDataFile, sep='\t', header = T, stringsAsFactors = F)
 
 # subset by if has WES data
-toUse <- data[which(!is.na(data$WES_ID)),]$WES_ID
-merged <- merged[which(merged$WES_ID %in% toUse),]
+toUse <- sampleData[which(!is.na(sampleData$WES_ID)),]$WES_ID
+sampleData <- sampleData[which(sampleData$WES_ID %in% toUse),]
 
 # subset by sample type if desired
 if (requireSM){
-  merged <- merged[which(merged$SampleType=='SM'),]
+  sampleData <- sampleData[which(sampleData$SampleType=='SM'),]
 }
 
+#######################################################################################
+### PART1 - Exploratory (only used for evaluating PyClone and FACETS purity)
+# set up structure just for exploratory
+merged <- sampleData 
+ 
 # specify purity metric to use
 merged$purity <- merged$PyClone
 
 # Filter based on purity and inspect numbers of samples per patient
-merged <- merged[which(merged$purity >= purityCutoff),]
+merged <- merged[which(merged$purity >= exploratoryPurityCutoff),]
 kable(table(merged$Patient)) %>%
         kable_styling(bootstrap_options = c("striped", "hover")) %>%
         row_spec(0, bold=TRUE, background = "#C2BFBA")
@@ -142,7 +148,7 @@ vafsPerSample <- data.frame(patient=character(),
 for (p in patientsToUse){
   patientID <- as.character(gsub('P','Patient',p))
   print(patientID)
-  avfFile <- paste0(dataPath,patientID,'.R.mutations.avf.txt')
+  avfFile <- file.path(dataPath,'mutations_avf',paste0(patientID,'.R.mutations.avf.txt'))
   
   # get vafs
   muts <- read.delim(avfFile, as.is=TRUE)
@@ -176,7 +182,7 @@ for (p in patientsToUse){
   colnames(muts.bin) <- gsub('_called','',colnames(muts.bin))
   
   # identify all samples with high enough purity for assessing what is tumor-wide for this patient
-  minPuritySamples <- merged[which(merged$Patient == p),]$sample_type
+  minPuritySamples <- merged[which(merged$Patient == p & merged$purity >= exploratoryPurityCutoff),]$sample_type
   
   # determines which mutations are tumor-wide and which are not
   muts.bin.subset <- muts.bin[,minPuritySamples]
@@ -194,7 +200,164 @@ for (p in patientsToUse){
 }
 
 ## Output to files
-cutofftag <- paste0('_',gsub('[.]','p',purityCutoff))
+cutofftag <- paste0('_',gsub('[.]','p',exploratoryPurityCutoff))
 write.table(SNVcategoryPerPatient, file=paste0(outputPath,outfolder,tag,cutofftag,'_min_purity_',samplesPerPatientToUse,'_min_samples_snv_categories.txt'),sep='\t', quote=F, row.names = F)
 write.table(vafsPerSample, file=paste0(outputPath,outfolder,tag,'_vafs.txt'),sep='\t', quote=F, row.names = F)
+
+#######################################################################################
+
+### PART 2: Used for all other mutation classification in the manuscript, which are done using PyClone
+
+# specify purity metric to use
+sampleData$purity <- sampleData$PyClone
+sampleData[sampleData$PurityEstUsed=='FACETS',]$purity <- sampleData[sampleData$PurityEstUsed=='FACETS',]$FACETS
+
+# Set final patients to use
+patientsToUse <- patientOrder
+
+# Read in pyclone clone cellular prevalence data 
+pycloneClone <- read.table(file.path(dataPath, 'pycloneiv_100rr_40cl_results_cluster_abundance.tsv'), sep='\t', header=T)
+
+# Read in pyclone variant to clone data
+pycloneVar <- read.table(file.path(dataPath, 'pycloneiv_100rr_40cl_results_variant_to_cluster.tsv'), sep='\t', header=T)
+
+# Create an empty data frame for storing the number of total mutations called per patient that are clonal, shared, or private
+vafsPerSample <- c()
+colNames <- c('patient','sampleID','SNVuniqueID','type','SangerCancerGeneCensus-','vaf','mut_called','mut_category','mut_category_used','mut_category_nsamp','cluster_id','cellular_prevalence','cellular_prevalence_std','upper_95CI','lower_95CI','pyclone_called','clonal','pyclone_category','pyclone_category_used','pyclone_category_nsamp','cluster_assignment_prob')
+
+for (p in patientsToUse){
+  patientID <- as.character(gsub('P','Patient',p))
+  print(patientID)
+  avfFile <- file.path(dataPath,'mutations_avf',paste0(patientID,'.R.mutations.avf.txt'))
+  
+  # get vafs
+  muts <- read.delim(avfFile, as.is=TRUE)
+  muts <- muts[muts$decision=='retain',]
+  if('X.gene' %in% colnames(muts)){
+    geneIndex <- grep('X.gene',colnames(muts))
+    colnames(muts)[geneIndex] <- 'gene'
+  }
+  muts <- muts[which(muts$algorithm == "MuTect"),]
+  muts$SNVuniqueID <- paste(muts$gene, gsub('chr','',muts$contig), muts$position, muts$ref_allele, muts$alt_allele, sep="_")
+  rownames(muts) <- muts$SNVuniqueID
+  colnames(muts) <- gsub('[.]','-',colnames(muts))
+  alt_counts <- muts[,grepl('_alt_Q20reads', colnames(muts))]
+  colnames(alt_counts) <- gsub('_alt_Q20reads','',colnames(alt_counts))
+  ref_counts <- muts[,grepl('_ref_Q20reads', colnames(muts))]
+  colnames(ref_counts) <- gsub('_ref_Q20reads','',colnames(ref_counts))
+  vafs <- alt_counts/(alt_counts+ref_counts)
+  vafs$Normal <- NULL
+  vafs$SNVuniqueID <- rownames(vafs)
+  
+  # melt data structure and add in patient info
+  vafsPerSample_Local <- melt(vafs, id.vars='SNVuniqueID')
+  colnames(vafsPerSample_Local) <- c('SNVuniqueID','sampleID','vaf')
+  vafsPerSample_Local$patient <- p
+  
+  # run muts.bin function, which creates the same logical matrix used for phylo trees
+  muts.bin <- getMutsBin(avfFile) #muts.bin for all samples in the patient, not yet subsetted
+  
+  # make muts.bin SNV unique ID compatible downstream
+  rownames(muts.bin) <- gsub('_chr','_',rownames(muts.bin))
+  
+  # make the column names so they can string match our sample names
+  colnames(muts.bin) <- gsub('[.]','-',colnames(muts.bin))
+  colnames(muts.bin) <- gsub('_called','',colnames(muts.bin))
+  
+  # identify all samples with high enough purity for assessing what is tumor-wide for this patient
+  minPurityCallsSamples <- sampleData[which(sampleData$Patient == p & sampleData$purity >= finalPurityCutoffCalls),]$sample_type
+  
+  # determines which mutations are tumor-wide and which are not
+  muts.bin.subset <- muts.bin[,minPurityCallsSamples]
+  tumorWideSNVs <- rownames(muts.bin.subset[which(rowSums(muts.bin.subset)== ncol(muts.bin.subset)),])
+  privateSNVs <- rownames(muts.bin.subset[which(rowSums(muts.bin.subset)== 1),])
+  sharedSNVs <- rownames(muts.bin.subset[which(rowSums(muts.bin.subset) < ncol(muts.bin.subset) & rowSums(muts.bin.subset) > 1),])
+  category <- c(rep('tumor-wide',length(tumorWideSNVs)), rep('shared',length(sharedSNVs)), rep('private',length(privateSNVs)))
+  mut_category_nsamp <- ncol(muts.bin.subset)
+  SNVcategoryPerPatient_Local <- as.data.frame(cbind(SNVuniqueID=c(tumorWideSNVs, sharedSNVs, privateSNVs), mut_category=category, mut_category_nsamp=mut_category_nsamp))
+  
+  # melt muts.bin to get call
+  muts.bin$SNVuniqueID <- row.names(muts.bin)
+  muts.binPerSample_Local <- melt(muts.bin, id.vars='SNVuniqueID')
+  colnames(muts.binPerSample_Local) <- c('SNVuniqueID','sampleID','mut_called')
+  
+  # add distribution of calls to call (mut-level)
+  muts.binPerSample_Local <- merge(SNVcategoryPerPatient_Local, muts.binPerSample_Local, by='SNVuniqueID', all=TRUE)
+  
+  # add call information to vafs
+  vafsPerSample_Local$SNVuniqueIDSamp <- paste0(vafsPerSample_Local$SNVuniqueID,'_',vafsPerSample_Local$sampleID)
+  muts.binPerSample_Local$SNVuniqueIDSamp <- paste0(muts.binPerSample_Local$SNVuniqueID,'_',muts.binPerSample_Local$sampleID)
+  vafsPerSample_Local <- merge(vafsPerSample_Local, muts.binPerSample_Local[,c('SNVuniqueIDSamp','mut_called','mut_category','mut_category_nsamp')], by='SNVuniqueIDSamp', all=T)
+  
+  # annotate whether sample calls were used to decide to determine mut_category
+  vafsPerSample_Local$mut_category_used <- ifelse(vafsPerSample_Local$sampleID %in% minPurityCallsSamples, TRUE, FALSE)
+  
+  # add sanger cancer annotation
+  vafsPerSample_Local <- merge(vafsPerSample_Local, muts[,c('SNVuniqueID','SangerCancerGeneCensus-','type')], by='SNVuniqueID', all=T)
+
+  # bring in pyclone clonal abundance per sample
+  pycloneClone_Local <- pycloneClone[pycloneClone$Patient==patientID,]
+  
+  # determine whether prevalence  @ lower 95CI is over threshold
+  pycloneClone_Local$pyclone_called <- pycloneClone_Local$lower_95CI > pycloneCellPrevCutoff
+  
+  # create call matrix
+  pyclone.bin <- spread(pycloneClone_Local[,c('sample_id','cluster_id','pyclone_called')], sample_id, pyclone_called )
+  rownames(pyclone.bin) <- pyclone.bin$cluster_id
+  pyclone.bin$cluster_id <- NULL
+  
+  # subset to remove low purity samples if needed
+  minPurityNonzeroSamples <- sampleData[which(sampleData$Patient == p & sampleData$purity >= finalPurityCutoffNonzero),]$sample_type
+  pyclone.bin.subset <- pyclone.bin[,minPurityNonzeroSamples] # currently using all
+  tumorWideClusters <- rownames(pyclone.bin.subset[which(rowSums(pyclone.bin.subset)== ncol(pyclone.bin.subset)),])
+  privateClusters <- rownames(pyclone.bin.subset[which(rowSums(pyclone.bin.subset)== 1),])
+  sharedClusters <- rownames(pyclone.bin.subset[which(rowSums(pyclone.bin.subset) < ncol(pyclone.bin.subset) & rowSums(pyclone.bin.subset) > 1),])
+  category <- c(rep('tumor-wide',length(tumorWideClusters)), rep('shared',length(sharedClusters)), rep('private',length(privateClusters)))
+  pyclone_category_nsamp <- ncol(pyclone.bin.subset)
+  PyClonePerPatient_Local <- as.data.frame(cbind(cluster_id=c(tumorWideClusters, sharedClusters, privateClusters), pyclone_category=category, pyclone_category_nsamp=pyclone_category_nsamp))
+
+  # add categories to original cluster structure
+  pycloneClone_Local <- merge(PyClonePerPatient_Local, pycloneClone_Local, by='cluster_id', all=TRUE)
+  
+  # bring in pyclone variant to clone mapping
+  pycloneVar_Local <- pycloneVar[pycloneVar$Patient==patientID,]
+  
+  # add variant clone mapping to clonal abundace
+  pycloneAll_Local <- merge(pycloneClone_Local[,c('cluster_id','sample_id','cellular_prevalence','cellular_prevalence_std','upper_95CI','lower_95CI','pyclone_called','clonal','pyclone_category','pyclone_category_nsamp')], pycloneVar_Local[,c('mutation_id','cluster_id','cluster_assignment_prob')], by='cluster_id', all=T)
+  
+  # annotate whether sample calls were used to decide pyclone_category
+  pycloneAll_Local$pyclone_category_used <- ifelse(pycloneAll_Local$sample_id %in% minPurityNonzeroSamples, TRUE, FALSE)
+  
+  # add pyclone to vafs
+  pycloneAll_Local$SNVuniqueIDSamp <- paste0(pycloneAll_Local$mutation_id,'_',pycloneAll_Local$sample_id)
+  pycloneAll_Local$sample_id <- NULL
+  pycloneAll_Local$mutation_id <- NULL
+  vafsPerSample_Local <- merge(vafsPerSample_Local, pycloneAll_Local, by='SNVuniqueIDSamp', all.x=T, all.y=F) # we want everything with a vaf, but nothing in PyClone not in VAFs 
+  
+  # clean up final data structure
+  vafsPerSample_Local$SNVuniqueIDSamp <- NULL
+  vafsPerSample_Local <- vafsPerSample_Local[,colNames]
+  
+  # remove any samples not in the final sample set
+  sampleData_Local <- sampleData[sampleData$Patient==p,]
+  vafsPerSample_Local <- vafsPerSample_Local[vafsPerSample_Local$sampleID %in% sampleData_Local$sample_type,]
+  
+  # add to patient-level information about vafs for each sample to data frame
+  vafsPerSample <- rbind(vafsPerSample, vafsPerSample_Local)
+  
+}
+
+# Check all looks as expected
+sum(is.na(vafsPerSample$patient))
+sum(is.na(vafsPerSample$sampleID))
+sum(is.na(vafsPerSample$SNVuniqueID))
+sum(is.na(vafsPerSample$mut_called)) # some will be NA because not covered in all, is okay
+sum(is.na(vafsPerSample$vaf))
+sum(is.na(vafsPerSample$pyclone_called)) # many will be NA here because there are variants that couldn't be used by PyClone
+
+## Output to files
+cutofftag <- paste0('_',gsub('[.]','p',finalPurityCutoff))
+write.table(vafsPerSample, file=paste0(outputPath,outfolder,tag,cutofftag,'_min_final_purity_samples_snv_vafs_categories.txt'),sep='\t', quote=F, row.names = F)
+
+
 
